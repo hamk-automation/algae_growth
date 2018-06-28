a922fd9468416b6483583ea56cbdda2f97e43ac8
anhtumai <tu.mai@student.hamk.fi>
2018-06-28 07:31:14 +0000

    change a little

--- Server/app.js
+++ Server/app.js
@@ -18,92 +18,48 @@ app.use(session({
 }));
 app.use(function(req, res, next){
    // res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
+   res.locals.error   = req.flash("error");
    res.locals.success = req.flash("success");
    next();
 });
-// app.use(cookieParser('keyboard cat'));
-// app.use(session({ cookie: { maxAge: 60000 }}));
-// app.use(flash());
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-// app.use(cookieParser('secret'));
-//app.use(express.session({ cookie: { maxAge: 60000 }}));
 app.set('view engine', 'ejs');
 app.get('/',(req,res)=>{
   res.render('index2');
-  // while(true){
-  //   continue;
-  // }
 });
 io.on('connect',function(socket){
   console.log("Open the socket");
   app.post('/new_ph',(req,res)=>{
-    socket.removeAllListeners("success");
-    socket.removeAllListeners("error");
+    console.time('test');
     position = Object.getOwnPropertyNames(req.body)[0];
     console.log(position);
     console.log(typeof(position));
     io.emit('welcome', { "data":position });
     socket.on('success',function(){
       console.log("success");
-      req.flash("success","Successful Calibration !!!");
-      res.redirect('/bio');
+      req.flash("success","Successful Calibration with " + position + " position !!!");
+      console.log(res.locals);
+      // res.redirect('/bio');
       //socket.removeAllListeners("success");
     });
     socket.on('error',function(){
       console.log("error");
       req.flash("error","Some error exists!! Try to recalibrate !!!");
-      res.redirect('/bio');
+      // res.redirect('/bio');
       //socket.removeAllListeners("error");
     });
-    // console.log("before");
-    // res.redirect('/bio');
-    // socket.on('error',()=>{
-    //   console.log("error");
-    // });
-
+    setTimeout(function(){
+      res.redirect('/bio');
+      socket.removeAllListeners("success");
+      socket.removeAllListeners("error");
+      console.timeEnd('test');
+    },2500)
   });
 
 });
-// app.post('/new_ph',(req,res)=>{
-//   position = Object.getOwnPropertyNames(req.body)[0];
-//   console.log(position);
-//   console.log(typeof(position));
-//   io.emit('welcome', { "data":position });
-//   res.redirect('/bio');
-// });
-// io.on('connect', function (socket) {
-//   console.log("Socket is open");
-//   socket.on('success', function () {
-//     // req.flash('success', 'This is a flash message using the express-flash module.');
-//     console.log("success");
-//     console.log(socket.handshake);
-//   });
-//   socket.on('error', function () {
-//     console.log("error");
-//   });
-// });
-// io.on('success',function(){
-// console.log("Outside");
-// });
-// io.on('connection', function (socket) {
-//   socket.on('success', function () {
-//     // req.flash('success', 'This is a flash message using the express-flash module.');
-//     console.log("success");
-//     console.log(socket.handshake);
-//   });
-//   socket.on('error', function () {
-//     console.log("error");
-//   });
-// });
-// app.use(function(req, res, next){
-//    res.locals.error = req.flash("error");
-//    res.locals.success = req.flash("success");
-//    next();
-// });
 
 server.listen(process.env.PORT || 9000, process.env.IP, function(){
     console.log("SERVER IS RUNNING!");
